2aea17dd12e24347c48e3ce2120b74ea3ebed1ff
LittlePox <yuanyuan1992819@126.com>
2019-08-25 22:32:14 +0800

    Updated readme

--- OKEGui/OKEGui/Gui/MainWindow.xaml.cs
+++ OKEGui/OKEGui/Gui/MainWindow.xaml.cs
@@ -11,8 +11,10 @@ namespace OKEGui
     /// MainWindow.xaml 的交互逻辑
     /// 相关概念解释:
     ///   Task: 从单个源（例如m2ts）到成品（例如mkv）的整个过程。task会在主程序界面的列表里显示。
-    ///   Job: 每个task会被分解成不同的Jobs，并依次执行。例如抽流，压制，封装等。Job是可以独立运行的最低单位。
-    ///   Worker: 每一个task只会在一个worker里进行，因此有几个worker就允许几个task同时进行。多开相关的选项。
+    ///   Job: 每个Task会被分解成不同的Job，并依次执行。例如抽流，压制，封装等。Job是可以独立运行的最低单位。
+    ///   JobProcessror: 负责执行每个Job的命令行Warpper。比如X265Encoder调用x265压制HEVC，FFMpegVolumeChecker调用ffmpeg检查音轨音量
+    ///   Model: 储存媒体文件相关的信息。Info只带例如语言、封装选项等信息，Track则是File+Info的组合，MediaFile则是多条Track的合集
+    ///   Worker: 每一个Task只会在一个Worker里进行，因此有几个Worker就允许几个Task同时进行。多开相关的选项。每个Task具体的实现流程由Worker负责执行。
     /// </summary>
     public partial class MainWindow : Window
     {
