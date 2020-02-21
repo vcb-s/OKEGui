f25c760a9e40ca7b84902c3e6e057183d183dccc
LittlePox <yuanyuan1992819@126.com>
2020-02-21 23:48:02 +0800

    cleanup.

--- OKEGui/OKEGui/JobProcessor/RpChecker/RpChecker.cs
+++ OKEGui/OKEGui/JobProcessor/RpChecker/RpChecker.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
 using OKEGui.JobProcessor;
 using OKEGui.Utils;
