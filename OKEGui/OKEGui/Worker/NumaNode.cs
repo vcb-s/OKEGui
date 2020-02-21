b987d82a2b03a4956d29dc39c7951a40e60e502e
LittlePox <yuanyuan1992819@126.com>
2020-02-22 00:16:49 +0800

    cleanup

--- OKEGui/OKEGui/Worker/NumaNode.cs
+++ OKEGui/OKEGui/Worker/NumaNode.cs
@@ -1,10 +1,7 @@
 ﻿using OKEGui.Utils;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OKEGui.Worker
 {
