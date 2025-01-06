88110474c8892f99fd5eaff89a559bbeea909af3
sinsanction <1602723930@qq.com>
2025-01-06 15:17:43 +0800

    SingleVideoMuxer: fix the problem that the end frame is missing after splitting in some extreme cases according to mkvmerge's splitting feature

--- OKEGui/OKEGui/JobProcessor/Muxer/SingleVideoMuxer.cs
+++ OKEGui/OKEGui/JobProcessor/Muxer/SingleVideoMuxer.cs
@@ -25,7 +25,12 @@ namespace OKEGui.JobProcessor
             commandLine += $" --default-track \"0:1\" --language 0:und \"(\" \"{MJob.Input}\" \")\"";
             if (MJob.IsPartialMux)
             {
-                commandLine += $" --split parts-frames:{MJob.FrameRange.begin + 1}-{MJob.FrameRange.end}";
+                // OKE's frame number range is in python format, with the start frame included but the end frame excluded.
+                // So when converting to mkvmerge format, only the start frame number is added by one.
+                // According to mkvmerge's documentation, when splitting by frame, the result will contain the content up to but excluding the first key frame at or after the end frame.
+                // It means that if the end frame happens to be a key frame (there are two consecutive key frames in end and end+1), the end frame won't be included.
+                // To avoid such a situation, we pass end+1 to mkvmerge, since end+1 frame is always a key frame and it won't be included.
+                commandLine += $" --split parts-frames:{MJob.FrameRange.begin + 1}-{MJob.FrameRange.end + 1}";
             }
         }
     }
